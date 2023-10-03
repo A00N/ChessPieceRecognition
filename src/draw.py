81f552a59bc5c09385e1ff09c0b713a4630c5f46
Nooa Saareks <nsaareks@gmail.com>
2023-10-03 15:12:49 +0300

    Add centralizing mass to draw.py

--- src/draw.py
+++ src/draw.py
@@ -2,12 +2,14 @@ import tkinter as tk
 from PIL import Image, ImageDraw, ImageOps
 import numpy as np
 import network
+import cv2
 
 class DigitRecognizerApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Digit Recognizer")
-        self.size = 112
+        self.size = 560
+        self.brush = 20
 
         self.canvas = tk.Canvas(root, width=self.size, height=self.size, bg="black")  # Set the canvas background to black
         self.canvas.pack()
@@ -42,28 +44,39 @@ class DigitRecognizerApp:
             x, y = event.x, event.y
             # Draw a small oval in white
             self.canvas.create_oval(
-                x - 5, y - 5, x + 5, y + 5,
+                x - self.brush, y - self.brush, x + self.brush, y + self.brush,
                 fill="white", outline="white"
             )
             # Draw on the image as well
-            self.draw.ellipse([x - 5, y - 5, x + 5, y + 5], fill="white", outline="white")
+            self.draw.ellipse([x - self.brush, y - self.brush, x + self.brush, y + self.brush], fill="white", outline="white")
 
     def clear_canvas(self):
         self.canvas.delete("all")
-        self.image = Image.new("L", (112, 112), "black")
+        self.image = Image.new("L", (self.size, self.size), "black")
         self.draw = ImageDraw.Draw(self.image)
         self.label.config(text="")
 
     def predict_digit(self):
-        # Resize and normalize the drawn image
+        # Resize the drawn image to 28x28 pixels
         resized_image = self.image.resize((28, 28), Image.BICUBIC)
-        centered_image = Image.new("L", (28, 28), "black")
-        centered_image.paste(resized_image, (0, 0))
-        centered_image = np.array(centered_image)
+        # Convert the resized image to a numpy array
+        resized_image_array = np.array(resized_image)
+        # Calculate the center of mass (centroid) of the digit image
+        M = cv2.moments(resized_image_array)
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00'])
+        # Calculate the offset needed to move the center of mass to the center of the canvas
+        offset_x = 14 - cx  # 14 is the desired center in a 28x28 canvas
+        offset_y = 14 - cy
+        # Create a translation matrix
+        translation_matrix = np.float32([[1, 0, offset_x], [0, 1, offset_y]])
+        # Apply the offset to the digit image
+        centered_image = cv2.warpAffine(resized_image_array, translation_matrix, (28, 28))
         centered_image = centered_image.reshape((28 * 28, 1)) / 255.0
-
         predicted_digit = np.argmax(self.net.feedforward(centered_image))
         self.label.config(text=f"Predicted Digit: {predicted_digit}")
+
+
 
 if __name__ == "__main__":
     root = tk.Tk()
